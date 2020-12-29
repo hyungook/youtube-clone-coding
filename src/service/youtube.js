4f72075557ebebfac9cda5816d90c14d4b22421d
오형욱 <nenonenoberneno@gmail.com>
2020-12-29 21:13:01 +0900

    add youtube service

---
+++ src/service/youtube.js
@@ -0,0 +1,24 @@
+class Youtube {
+    constructor(key) {
+        this.key = key;
+        this.getRequestOption = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+    }
+
+    async mostPopular() {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&key=${this.key}`, this.RequestOptions);
+        const result = await response.json();
+        return result.items;
+    };
+
+    async search(query) {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search/?part=snippet&maxResults=30&q=${query}&type=video&key=${this.key}`, this.RequestOptions);
+        const result = await response.json();
+        return result.items.map(item => ({ ...item, id: item.id.videoId }));
+    };
+
+}
+
+export default Youtube;
