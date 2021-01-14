b411b7b78b5346f5ec5cc60ccff7d3c745ac94f4
오형욱 <nenonenoberneno@gmail.com>
2021-01-14 20:36:38 +0900

    Restart show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,26 @@
-import { useEffect, useState } from 'react';
-import styles from './App.module.css';
-import SearchHeader from './components/search_header/search_header';
+import React, { useEffect, useState } from 'react';
 import VideoList from './components/video_list/video_list';
+// import './app.css';
 
-function App({youtube}) {
+function App() {
+  // 데이터를 받아올 state 설정
   const [videos, setVideos] = useState([]);
-  const search = (query) => {
-    youtube.search(query) //
-    .then(videos => setVideos(videos));
-  };
 
+  // 데이터를 마운트 되었거나, 업데이트가 될 때 사용할 수 있는 콜백을 등록 가능  // useEffect()
   useEffect(() => {
-    youtube.mostPopular() //
-    .then(videos => setVideos(videos));
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&key=AIzaSyDNG_hfL-YnQr3sh-KrE0yRiVEdJzIgMP4", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
   }, []);
-  return <div className={styles.app}>
-    <SearchHeader onSearch={search}/>
-    <VideoList videos={videos} />
- </div>
+
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
