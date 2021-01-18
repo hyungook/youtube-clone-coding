e539366e71de03782d8bf361d56a0230f13be716
오형욱 <nenonenoberneno@gmail.com>
2021-01-18 20:24:46 +0900

    refactoring

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 import styles from './app.module.css';
 import VideoDetail from './components/video_detail/video_detail';
 
-function App({youtube}) {
+function App({ youtube }) {
   // 데이터를 받아올 state 설정
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
-
 
   const selectVideo = (video) => {
     setSelectedVideo(video);
   };
 
-  const search = query => {
+  const search = useCallback(query => {
     setSelectedVideo(null);
-    
+
     youtube
       .search(query) //
       .then(videos => {
         setVideos(videos);
         // setSelectedVideo(null);
       });
-  };
+  }, []);
 
   // 데이터를 마운트 되었거나, 업데이트가 될 때 사용할 수 있는 콜백을 등록 가능  // useEffect()
   useEffect(() => {
     youtube
       .mostPopular() //
       .then(videos => setVideos(videos));
-  }, []);
+  }, [youtube]);
 
 
   return (
--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -6,8 +6,9 @@ const VideoDetail = ({video, video: {snippet}}) => (
         <iframe 
         className={styles.video}
         type="text/html" width="100%" height="500px"
+        title="youtube video player"
         src={`https://www.youtube.com/embed/${video.id}`}
-        frameborder="0" allowfullscreen></iframe>
+        frameBorder="0" allowFullScreen></iframe>
         <h2>{snippet.title}</h2>
         <h3>{snippet.channelTitle}</h3>
         <pre className={styles.description}>{snippet.description}</pre>
